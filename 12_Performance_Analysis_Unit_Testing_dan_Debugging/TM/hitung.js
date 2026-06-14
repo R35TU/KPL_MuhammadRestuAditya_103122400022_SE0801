a25c374fee4ff0717ac57e84ec8e3288fd4ab787
Muhammad Restu Aditya <restuaditya.vv@gmail.com>
2026-06-14 19:18:09 +0700

    feat: Tugas Mandiri Modul 12

---
+++ 12_Performance_Analysis_Unit_Testing_dan_Debugging/TM/hitung.js
@@ -0,0 +1,15 @@
+// Kode kurang tepat
+
+/*
+function tambahPengitung(terkini, jumlah) {
+  terkini = terkini + jumlah;
+  return terkini;
+}
+*/
+
+// Kode yang tepat
+function tambahPengitung(terkini, jumlah) {
+  return terkini + jumlah;
+}
+
+export { tambahPengitung };
